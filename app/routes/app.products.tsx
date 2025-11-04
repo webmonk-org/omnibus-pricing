af6a4aa0da8325473c34f0e442c22fd648fb36ff
ali-rhou-wm <ali.rhou@webmonk.io>
2025-11-04 13:02:22 +0100

    added products page

---
+++ app/routes/app.products.tsx
@@ -0,0 +1,11 @@
+import { Page, Text } from "@shopify/polaris";
+
+export default function Products() {
+  return (
+    <Page>
+      <Text as="p">
+        Products page goes here.
+      </Text>
+    </Page>
+  )
+}
